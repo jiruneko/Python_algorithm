3b6a02eeb4f2c3a42ece8f64f99689d86ccf4d56
jiruneko <oohikaru310@gmail.com>
2025-05-25 07:16:13 +0900

    first commit

---
+++ ch1/99debug.py
@@ -0,0 +1,6 @@
+for n in range(1, 9+1):
+    print('n=', n)
+    
+    for m in range(1, 9+1):
+        value = n * m
+        print('', n, '*', m, '=', value)
---
+++ sum1to10.py
@@ -0,0 +1,5 @@
+N = 10
+total = 0
+for i in range(1, N + 1):
+    total += 1
+print(total)
